5772008fe60dc3dbfb8556bb53f02296f080a9e4
Roberto Alves Ferreira Filho <robertoaff06@hotmail.com>
2020-06-05 02:25:35 -0300

    Carregando logo?

--- gobarber-web/src/pages/SignIn/Index.tsx
+++ gobarber-web/src/pages/SignIn/Index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Container, Content, Background } from './styles';
+// import logo from '../../assets/img/logo.svg';
 
 const SignIn: React.FC = () => (
   <>
-    <h1>SignIN</h1>
+    <Container>
+      <h1>Logo</h1>
+      <Content>{/* <img src={logo} />{' '} */}</Content>
+      <Background />
+    </Container>
   </>
 );
 
