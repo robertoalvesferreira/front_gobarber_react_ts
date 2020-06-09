0cb3eec38222f390a636fa9974786bd00f661213
Roberto Alves Ferreira Filho <robertoaff06@hotmail.com>
2020-06-08 21:22:35 -0300

    Isolando componentes Input e Button

--- gobarber-web/src/pages/SignIn/Index.tsx
+++ gobarber-web/src/pages/SignIn/Index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { shade } from 'polished';
 import { Container, Content, Background } from './styles';
-// import logo from '../../assets/img/logo.svg';
+import logo from '../../assets/img/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <>
     <Container>
-      <h1>Logo</h1>
-      <Content>{/* <img src={logo} />{' '} */}</Content>
+      <Content>
+        <img src={logo} alt="GoBarber" />
+        <form>
+          <h1>Faça seu logon</h1>
+          <Input name="email" type="text" icon={FiMail} placeholder="Email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+        </form>
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
       <Background />
     </Container>
   </>
--- gobarber-web/src/pages/SignIn/styles.ts
+++ gobarber-web/src/pages/SignIn/styles.ts
@@ -1,5 +1,64 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+import signInBackgroundImg from '../../assets/img/sign-in-background.png';
 
-export const Container = styled.div``;
-export const Content = styled.div``;
-export const Background = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+  place-content: center;
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+
+      e:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 15px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
