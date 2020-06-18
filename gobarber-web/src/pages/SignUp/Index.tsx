0f05621f766ed32f20ea07c5c96bb4b9a94b2121
Roberto Alves Ferreira Filho <robertoaff06@hotmail.com>
2020-06-17 22:14:16 -0300

    Adicionando o FORM

--- gobarber-web/src/pages/SignUp/Index.tsx
+++ gobarber-web/src/pages/SignUp/Index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/img/logo.svg';
-
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input name="name" type="text" icon={FiUser} placeholder="Email" />
           <Input name="email" type="text" icon={FiMail} placeholder="Email" />
@@ -23,14 +27,14 @@ const SignIn: React.FC = () => (
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="login">
           <FiArrowLeft />
           Voltar para logon
         </a>
       </Content>
     </Container>
-  </>
-);
+  );
+};
 
-export default SignIn;
+export default SignUp;
