3d91f1a4a528785f4356385280e5a517eddda3fa
Roberto Alves Ferreira Filho <robertoaff06@hotmail.com>
2020-06-17 22:55:28 -0300

    Implementando css condicional de acordo com stado do input

--- gobarber-web/src/components/Input/styles.ts
+++ gobarber-web/src/components/Input/styles.ts
@@ -1,18 +1,40 @@
 import styled from 'styled-components';
+import { borderColor } from 'polished';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 8px;
   width: 100%;
+
+  border: 2px solid #232129;s
   color: #666360;
+
   display: flex;
   align-items: center;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) =>
+    props.isFocused &&
+    `
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    `
+      color: #ff9000;
+    `}
+    
 
   input {
     flex: 1;
