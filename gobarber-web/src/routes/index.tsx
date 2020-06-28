0bde07e340c7fa55781909122aa6e9079534977d
Roberto Alves Ferreira Filho <robertoaff06@hotmail.com>
2020-06-28 18:53:38 -0300

    Colocando rotas privadas

--- gobarber-web/src/routes/index.tsx
+++ gobarber-web/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 import SignIn from '../pages/SignIn/Index';
 import SignUp from '../pages/SignUp/Index';
+import Dashboard from '../pages/Dashboard/index';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    <Route path="/signUp" exact component={SignUp} />
+    <Route path="/signUp" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
